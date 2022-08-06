b0ac5937081bc707b8c1f4c847a540c30bdc00d2
sup25 <suparnaa51@gmail.com>
2022-08-06 10:51:18 +0545

    updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
+const API_URL = "http://www.omdbapi.com?apikey=ed015dd";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -23,7 +23,7 @@ const App = () => {
 
   return (
     <div className="app">
-      <h1>MovieLand</h1>
+      <h1>Show Times</h1>
 
       <div className="search">
         <input
